efda443a1329250b94598eaac29d37b9fb0cf5f1
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-02 20:34:10 +0700

    feat(delete post): create service

--- app/service/post.service.js
+++ app/service/post.service.js
@@ -1,3 +1,5 @@
+import { ResponseError } from "../../exception/response.error.js";
+import { sequelize } from "../../infrastructure/database/db_connection.js";
 import { Post } from "../model/entity.model.js";
 import postValidation from "../validation/post.validation.js";
 import { validate } from "../validation/validation.js";
@@ -11,6 +13,44 @@ const create = async (user, request) => {
   });
 };
 
+const remove = async (user, request) => {
+  request = validate(postValidation.deletePostValidation, request);
+
+  try {
+    await sequelize.transaction(async (t) => {
+      const totalPostInDatabase = await Post.findAndCountAll(
+        {
+          where: {
+            userId: user.id,
+            id: request.postId,
+          },
+        },
+        {
+          transaction: t,
+        }
+      );
+
+      if (totalPostInDatabase === 0) {
+        throw new ResponseError(404, "post not found");
+      }
+
+      await Post.destroy(
+        {
+          where: {
+            id: request.postId,
+          },
+        },
+        {
+          transaction: t,
+        }
+      );
+    });
+  } catch (error) {
+    throw new ResponseError(err.status, err.message);
+  }
+};
+
 export default {
   create,
+  remove,
 };
