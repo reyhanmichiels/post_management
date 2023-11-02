ca5f0fc0bab90f6d559726e24e83b19209e90b9b
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-02 20:34:35 +0700

    feat(delete post): create handler

--- app/handler/post.handler.js
+++ app/handler/post.handler.js
@@ -15,6 +15,23 @@ const create = async (req, res, next) => {
   }
 };
 
+const remove = async (req, res, next) => {
+  try {
+    const request = req.body;
+    request.postId = req.params.postId;
+    await postService.remove(req.user, request);
+
+    req.statusCode = 200;
+    logResponse(req);
+    res.status(200).json({
+      data: "success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export default {
   create,
+  remove,
 };
