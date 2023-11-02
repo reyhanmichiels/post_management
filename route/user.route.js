04c8fd2c98ee8969a9b742fcb4b77e52b91a27fe
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-02 14:34:20 +0700

    feat(login): create login route

--- route/user.route.js
+++ route/user.route.js
@@ -4,5 +4,6 @@ import userHandler from "../app/handler/user.handler.js";
 const userApi = new express.Router();
 
 userApi.post("/api/users", userHandler.registration);
+userApi.post("/api/auth/login", userHandler.login);
 
 export { userApi };
