fffc7f7786c0e304f567915127dcdb17c2d95e04
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-02 14:34:01 +0700

    feat(login): create login handler

--- app/handler/user.handler.js
+++ app/handler/user.handler.js
@@ -15,6 +15,23 @@ const registration = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const result = await userService.login(req.body);
+
+    req.statusCode = 200;
+    logResponse(req);
+    res.status(200).json({
+      data: {
+        token: result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export default {
-    registration
-}
+  registration,
+  login,
+};
