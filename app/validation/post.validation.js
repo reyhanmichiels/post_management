310efde1332574acdfef3937e712bb3667dd35c5
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-02 20:33:43 +0700

    feat(delete post): create validation

--- app/validation/post.validation.js
+++ app/validation/post.validation.js
@@ -4,6 +4,11 @@ const createPostValidation = Joi.object({
   message: Joi.string().required(),
 });
 
+const deletePostValidation = Joi.object({
+  postId: Joi.number().positive().required(),
+});
+
 export default {
   createPostValidation,
+  deletePostValidation
 };
